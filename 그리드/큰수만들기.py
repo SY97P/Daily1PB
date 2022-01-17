5ec51d6a5de119198d84cb95114f08c756721566
SY97P <>
2022-01-17 03:12:03 +0000

    완전탐색법으로 풀이함 / 리스트 반환말고 조합 생성시마다 확인함으로 그리디 충족

--- 그리드/큰수만들기.py
+++ 그리드/큰수만들기.py
@@ -1,10 +1,17 @@
+from itertools import combinations
+
 def solution(number, k) :
-	
+	maxValue = -1
+	lst = list(map(''.join, combinations(number, len(number)-k)))
+	for item in lst : 
+		if (maxValue < int(item)) :
+			maxValue = int(item)
+	return str(maxValue)
 
 def main() :
-	number, k = "1924", 2
-	#number, k = "1231234", 3
-	#number, k = "4177252841", 4
+	number, k = "1924", 2			# 94
+	#number, k = "1231234", 3  		# 3234
+	number, k = "4177252841", 4 	# 775841
 	print("solution : ", solution(number, k))
 
 main()
