0569aa41567a72a087689ea339fd87177121d801
SY97P <>
2022-01-17 03:02:01 +0000

    큰수만들기 문제 디렉토리 구성

---
+++ 그리드/큰수만들기.py
@@ -0,0 +1,10 @@
+def solution(number, k) :
+	
+
+def main() :
+	number, k = "1924", 2
+	#number, k = "1231234", 3
+	#number, k = "4177252841", 4
+	print("solution : ", solution(number, k))
+
+main()
