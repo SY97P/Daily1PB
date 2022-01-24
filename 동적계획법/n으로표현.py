0c5b1b37bbe24452ad747f510fc55bb94ede17b6
SY97P <>
2022-01-24 05:32:31 +0000

    n으로표현 문제 풀이

---
+++ 동적계획법/n으로표현.py
@@ -0,0 +1,28 @@
+def solution(N, number) : 
+	answer = 0
+	if N == number : 
+		return 1
+	lst = [set() for i in range(8)]
+	for i in range(8) :
+		lst[i].add(int(str(N) * (i+1)))
+	# print(lst)
+	for i in range(1,8) :
+		for j in range(i) : 
+			for op1 in lst[j] :
+				for op2 in lst[i-j-1] :
+					lst[i].add(op1 + op2)
+					lst[i].add(op1 - op2)
+					lst[i].add(op1 * op2)
+					if op2 != 0 :
+						lst[i].add(op1 // op2)
+		if number in lst[i] : 
+			return i + 1
+	return -1
+
+def main() :
+	N, number = 5, 12	# 4
+	# N, number = 2, 11 	# 3
+	# N, number = 1, 1
+	print("solution : ", solution(N, number))
+
+main()
