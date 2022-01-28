64d8dc5c3f23601fb5dc64e9772d3757d9de0eb4
SY97P <>
2022-01-28 05:51:57 +0000

    여행경로 푸는 중.. 1번 TC가 안 풀림.

--- 깊이너비우선탐색/여행경로.py
+++ 깊이너비우선탐색/여행경로.py
@@ -1,31 +1,29 @@
 def solution(tickets) :
-	answer = []
-	# print(tickets)
+	answer = 0 
 	tickets.sort()
-	# print(tickets)
-	for ticket in tickets :
-		if ticket[0] == 'ICN' :
-			answer = dfsRoute(ticket, tickets)
-	return ['ICN'] + answer	
+	return getRoute('ICN', tickets)
 
-def dfsRoute(ticket, tickets) :
-	# print(ticket)
-	tickets.remove(ticket)
+def getRoute(begin, tickets) :
+	# print(begin, tickets)
+	if len(tickets) == 1 :
+		return [tickets[0][0], tickets[0][1]]
 	startlist = []
-	for t in tickets :
-		startlist.append(t[0])
-	if not tickets :
-		return [ticket[1]]
 	for t in tickets : 
-		if ticket[1] == t[0] and ticket[1] in startlist :
-			return [ticket[1]] + dfsRoute(t, tickets)
-	
+		startlist.append(t[0])
+	for t in tickets : 
+		if t[0] == begin and t[1] in startlist :
+			target = t[1]
+			tickets.remove(t)
+			return [begin] + getRoute(target, tickets)
 
 def main() :
 	tickets = [["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]	
 	# ["ICN", "JFK", "HND", "IAD"]
 	tickets = [["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]	
 	# ["ICN", "ATL", "ICN", "SFO", "ATL", "SFO"]
+	tickets = [["ICN", "AAA"],["ICN", "BBB"],["BBB", "ICN"]]
+	tickets =  [["ICN", "BOO"], ["ICN", "COO"], ["COO", "DOO"], ["DOO", "COO"], ["BOO", "DOO"], ["DOO", "BOO"], ["BOO", "ICN"], ["COO", "BOO"]]
+	# ["ICN", "BOO", "DOO", "BOO", "ICN", "COO", "DOO", "COO", "BOO"]
 	print("solution : ", solution(tickets))
 
 main()
