9ad370a7c5e1fdae338b15ed10a0c633a4cc95f8
SY97P <>
2022-01-31 09:23:04 +0000

    여행경로 내가 푼 거 아님.

--- 깊이너비우선탐색/여행경로.py
+++ 깊이너비우선탐색/여행경로.py
@@ -1,29 +1,46 @@
-def solution(tickets) :
-	answer = 0 
-	tickets.sort()
-	return getRoute('ICN', tickets)
+from collections import defaultdict
 
-def getRoute(begin, tickets) :
-	# print(begin, tickets)
-	if len(tickets) == 1 :
-		return [tickets[0][0], tickets[0][1]]
-	startlist = []
-	for t in tickets : 
-		startlist.append(t[0])
-	for t in tickets : 
-		if t[0] == begin and t[1] in startlist :
-			target = t[1]
-			tickets.remove(t)
-			return [begin] + getRoute(target, tickets)
+def solution(tickets) :
+	answer = []
+	routes = defaultdict(list)
+	print(routes)
+
+	for ticket in tickets :
+		routes[ticket[0]].append(ticket[1])
+	print(routes)
+
+	for key in routes.keys() :
+		routes[key].sort(reverse = True)
+	print(routes)
+
+	stack = ["ICN"]
+	
+	while stack :
+		tmp = stack[-1]
+		print("tmp : ", tmp)
+
+		if not routes[tmp] :
+			answer.append(stack.pop())
+			print("answer : ", answer)
+		else :
+			stack.append(routes[tmp].pop())
+			print("stack : ", stack)
+	answer.reverse()
+
+	return answer
+	
 
 def main() :
-	tickets = [["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]	
-	# ["ICN", "JFK", "HND", "IAD"]
+	tickets = [["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]	# ["ICN", "JFK", "HND", "IAD"]
+
 	tickets = [["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]	
 	# ["ICN", "ATL", "ICN", "SFO", "ATL", "SFO"]
+
 	tickets = [["ICN", "AAA"],["ICN", "BBB"],["BBB", "ICN"]]
+
 	tickets =  [["ICN", "BOO"], ["ICN", "COO"], ["COO", "DOO"], ["DOO", "COO"], ["BOO", "DOO"], ["DOO", "BOO"], ["BOO", "ICN"], ["COO", "BOO"]]
 	# ["ICN", "BOO", "DOO", "BOO", "ICN", "COO", "DOO", "COO", "BOO"]
+
 	print("solution : ", solution(tickets))
 
 main()
