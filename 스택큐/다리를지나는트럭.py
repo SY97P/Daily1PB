45a729cece4e111e49fd35335c74cef40c0a7740
SY97P <>
2022-02-14 05:29:24 +0000

    다리를 지나는 트럭 문제 풀이 : answer 가감하는 부분과 queue의 첫 원소를 빼주는 시점을 동일하게 제어하는 것이 코딩이나 문제 해결에 편리함. 또한 iterater 무지성 완전탐색보다 O(n) 함수인 pop(0)을 사용함이 더 효율적임.

--- 스택큐/다리를지나는트럭.py
+++ 스택큐/다리를지나는트럭.py
@@ -2,36 +2,27 @@ def solution(length, weight, trucks) :
 	answer = 0
 	queue = [0 for i in range(length)]
 
-	# print(len(trucks) * 5 + 1)
-
-	trucks.reverse()
-	while trucks : 
-		truck = trucks.pop()
-		# print(truck)
-		if sum(queue) == 0 : 
-			queue = queue[1:] + [truck]
-			answer += 1
-			print("answer : ", answer, "queue : ", queue)
-			continue
-		if sum(queue) + truck <= weight :
-			queue = queue[1:] + [truck]
-		while sum(queue) + truck > weight : 
-			queue = queue[1:] + [0]
-			answer += 1
-			print("answer : ", answer, "queue : ", queue)
-		# queue = queue[1:] + [truck]
-		# print("answer1 : ", answer, "queue : ", queue)
-	while sum(queue) != 0 :
+	while True : 
+		queue.pop(0)
 		answer += 1
-		queue = queue[1:] + [0]
-	return answer
+		if not trucks :
+			# print("answer : ", answer, "trucks : ", trucks)
+			break
+		if sum(queue) + trucks[0] <= weight : 
+			queue.append(trucks.pop(0))
+			# print("answer : ", answer, "queue : ", queue)
+		else : 
+			queue.append(0)
+			# print("answer : ", answer, "queue : ", queue)
+
+	return answer + length - 1
 
 
 def main() : 
 	length, weight, trucks = 2, 10, [7,4,5,6]	#8
-	#length, weight, trucks = 100, 100, [10]	#101
-	#length, weight, trucks = 100, 100, [10,10,10,10,10,10,10,10,10,10]	#110
-	#length, weight, trucks = 5, 5, [5, 5, 5, 5, 5, 5] # 31
+	length, weight, trucks = 100, 100, [10]	#101
+	length, weight, trucks = 100, 100, [10,10,10,10,10,10,10,10,10,10]	#110
+	length, weight, trucks = 5, 5, [5, 5, 5, 5, 5, 5] # 31
 	print("solution : ", solution(length, weight, trucks))
 
 main()
