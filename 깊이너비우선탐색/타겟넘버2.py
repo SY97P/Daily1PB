fbbaa3db1a882559f4fbc16abc6117a2624d54e2
SY97P <>
2022-03-11 08:10:31 +0000

    타겟넘버 개신기하넹

---
+++ 깊이너비우선탐색/타겟넘버2.py
@@ -0,0 +1,9 @@
+def solution(numbers, target) :
+	
+
+def main() : 
+	numbers, target = [1, 1, 1, 1, 1],3		# 5
+	numbers, target = [4, 1, 2, 1],4		# 2
+	print("solution : ", solution(numbers, target))
+
+main()
