7c10947cd87ef8a4ca69f2232e7bcf1ace395c93
SY97P <>
2022-03-15 08:17:20 +0000

    깊이너비우선탐색 여행경로 문제풀이 dfs로 품

--- 깊이너비우선탐색/여행경로2.py
+++ 깊이너비우선탐색/여행경로2.py
@@ -1,17 +1,46 @@
 def solution(tickets) :
-	answer = []
-	route = dict()
-	
-	route['a'] = "a"
-	route['a'].append("b")
+	size = len(tickets)
+	answer = dfs(size, tickets, "ICN")
+	return answer
 
-	print(route)
-			
+def dfs(size, tickets, start) :
+	# print("start : ", start)
+	candi = []
+	for t in tickets : 
+		if start == t[0] : 
+			candi.append(t)
+	# print("candi : ", candi)
+	if not candi :
+		# 마지막까지 왔지만 아직 남은 tickets가 있으면 돌아가야 함. 
+		if tickets : 
+			return ["null"]
+		return [start]
+	candi.sort(key = lambda x : x[1])
+	for c in candi : 
+		tickets.remove(c)
+		recursion = [start] + dfs(size, tickets, c[1])
+		# print(recursion)
+		if "null" not in recursion :
+			return recursion
+		tickets.append(c)
+	return ["null"]
 
 
 def main() :
-	tickets =  [["ICN", "BOO"], ["ICN", "COO"], ["COO", "DOO"], ["DOO", "COO"], ["BOO", "DOO"], ["DOO", "BOO"], ["BOO", "ICN"], ["COO", "BOO"]]
+	tickets = [["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]	
+	# ["ICN", "JFK", "HND", "IAD"]
+	
+	tickets = [["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], 						["ATL","SFO"]]	
+	# ["ICN", "ATL", "ICN", "SFO", "ATL", "SFO"]
+
+	tickets =  [["ICN", "BOO"], ["ICN", "COO"], ["COO", "DOO"], ["DOO", "COO"], 					["BOO", "DOO"], ["DOO", "BOO"], ["BOO", "ICN"], ["COO", "BOO"]]
 	# ["ICN", "BOO", "DOO", "BOO", "ICN", "COO", "DOO", "COO", "BOO"]
+
+	tickets = [["ICN", "ICN"],["ICN", "ICN"],["ICN", "ICN"]]
+
+	tickets = [["ICN", "AOO"], ["AOO", "BOO"], ["BOO", "COO"], ["COO", "DOO"], ["DOO", "EOO"], ["EOO", "DOO"], ["DOO", "COO"], ["COO", "BOO"], ["BOO", "AOO"]]
+	#["ICN", "AOO", "BOO", "COO", "DOO", "EOO", "DOO", "COO", "BOO", "AOO"]
+	# ["ICN", "AOO", "BOO", "COO", "DOO", "COO","BOO","AOO",null,null]
 
 	print("solution : ", solution(tickets))
 
